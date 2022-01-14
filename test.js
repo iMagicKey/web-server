456a1acaacb67dc5747cd34c1b77fb32ece0d5a8
iMagicKey <imagickey@icloud.com>
2022-01-14 22:04:29 +0300

    Test fix

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const WebServer = require('./index')
 let server = new WebServer({
     assets: [
         { route: '/assets/', dir: __dirname + '/' },
-        { route: '/123', file: __dirname + '/LICENSE' }
+        { route: '/LICENSE', file: __dirname + '/LICENSE' }
     ]
 })
 
-server.createRoute({ methods: ['GET'], url: '/hello' }, (res, req) => {
-    req.end('world')
+server.createRoute({ methods: ['GET'], url: '/hello' }, (req, res) => {
+    res.end('world')
 })
 server.listen(8080)
